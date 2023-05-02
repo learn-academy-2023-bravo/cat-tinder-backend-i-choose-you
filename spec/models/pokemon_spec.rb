8c5816ecb6a79843f8b4a23cf11603d7aa272625
Spencerj1297 <sajohnson1297@gmail.com>
2023-05-02 14:57:52 -0700

    finished validations minus stretch goals'

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -5,4 +5,27 @@ RSpec.describe Pokemon, type: :model do
     pokemon = Pokemon.create(evolve:'yes', evolution_stage:'2/3', typing: 'fire', pokedex_entry:'charmeleon, the Flame Pokémon and the evolved form of Charmander. Charmeleon knocks down opponents with its tail, then defeats them using razor-sharp claws.', images:'asdfghjkl')
     expect(pokemon.errors[:name]).to_not be_empty
   end
+  it "should validate if evolve exists in entry" do
+    pokemon = Pokemon.create(name:'Charmeleon', evolution_stage:'2/3', typing: 'fire', pokedex_entry:'charmeleon, the Flame Pokémon and the evolved form of Charmander. Charmeleon knocks down opponents with its tail, then defeats them using razor-sharp claws.', images:'asdfghjkl')
+    expect(pokemon.errors[:evolve]).to_not be_empty
+  end
+  it "should validate if evolution_stage exists in entry" do
+    pokemon = Pokemon.create(name:'Charmeleon', evolve:'yes', typing: 'fire', pokedex_entry:'charmeleon, the Flame Pokémon and the evolved form of Charmander. Charmeleon knocks down opponents with its tail, then defeats them using razor-sharp claws.', images:'asdfghjkl')
+    expect(pokemon.errors[:evolution_stage]).to_not be_empty
+  end
+  it "should validate if typing exists in entry" do
+    pokemon = Pokemon.create(name:'Charmeleon', evolve:'yes', evolution_stage: '2/3', pokedex_entry:'charmeleon, the Flame Pokémon and the evolved form of Charmander. Charmeleon knocks down opponents with its tail, then defeats them using razor-sharp claws.', images:'asdfghjkl')
+    expect(pokemon.errors[:typing]).to_not be_empty
+  end
+  it "should validate if pokedex_entry exists in entry" do
+    pokemon = Pokemon.create(name:'Charmeleon', evolve:'yes', evolution_stage: '2/3', typing:'fire', images:'asdfghjkl')
+    expect(pokemon.errors[:pokedex_entry]).to_not be_empty
+  end
+  it "should validate if images exists in entry" do
+    pokemon = Pokemon.create(name:'Charmeleon', evolve:'yes', evolution_stage: '2/3', typing:'fire', pokedex_entry:'charmeleon, the Flame Pokémon and the evolved form of Charmander. Charmeleon knocks down opponents with its tail, then defeats them using razor-sharp claws.')
+    expect(pokemon.errors[:images]).to_not be_empty
+  end
+  it "should have a pokedex_entry >= 10" do
+    pokemon = Pokemon.create(name:'Charmeleon', evolve:'yes', evolution_stage: '2/3', typing:'fire', pokedex_entry:'charmeleon', images:"dsafdsaf")
+  end
 end
